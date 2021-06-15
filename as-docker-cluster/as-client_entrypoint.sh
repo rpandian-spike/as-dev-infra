b7c1fd61f8082cae4aaa84e30b068bc302270f90
Ramaraj Pandian <rpandian@aerospike.com>
2021-06-15 11:17:12 -0700

    docker debugging support added

--- as-docker-cluster/as-client_entrypoint.sh
+++ as-docker-cluster/as-client_entrypoint.sh
@@ -10,6 +10,12 @@ echo "Node IP: $IP"
 
 #/usr/bin/python3 -m debugpy --listen 0.0.0.0:$DEBUGPY_PORT /usr/bin/odoo --db_user=odoo --db_host=db --db_password=odoo &
 
-/code/src/aerospike/enterprise/aerospike-admin/build/bin/asadm -e asadmn-setup -Uadmin -Padmin -h node1.asserver.com
+#cd /code/src/aerospike/enterprise/scan_partition-python
+#python3 /code/src/aerospike/enterprise/scan_partition-python/rp-setup.py build --force
+#python3 /code/src/aerospike/enterprise/scan_partition-python/rp-setup.py install --force
+
+sleep 2
+
+/code/src/aerospike/enterprise/aerospike-admin/build/bin/asadm -e asadmn-setup -Uadmin -Padmin -h as-s1.as-network.com
 
 wait

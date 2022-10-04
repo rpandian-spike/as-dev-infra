0f7fa797a349ac7056a2fc2f35d0879c1f272e4d
Ramaraj Pandian <rpandian@aerospike.com>
2022-10-04 17:13:10 +0530

    infra update

---
+++ as-docker-cluster/asadm-setup.sh
@@ -0,0 +1,3 @@
+/code/src/aerospike/enterprise/aerospike-admin/build/bin/asadm/asadm -e /code/src/aerospike/enterprise/as-dev-infra/as-docker-cluster/asadm-setup --enable -U admin -P admin -h 172.17.0.3 -p 3000
+
+
